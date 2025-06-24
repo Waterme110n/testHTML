0efe28780f6f5c1d23efc499590fdeea693398a6
Pavel Osadcy <pavelasadchy@gmail.com>
2025-06-24 19:47:09 +0300

    header-rework pt2

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 function toggleDropdown(button) {
-    var dropdown = button.nextElementSibling;
-    if (dropdown.style.display === "flex") {
-        dropdown.style.display = "none";
-        button.classList.remove('pressed');
-    } else {
-        closeAllDropdowns();    
-        dropdown.style.display = "flex";
-        button.classList.add('pressed');
-    }
+  var dropdown = button.nextElementSibling;
+
+  if (dropdown.classList.contains('dropdown-content-off')) {
+    dropdown.classList.remove('dropdown-content-off');
+    dropdown.classList.add('dropdown-content');
+    button.classList.add('pressed');
+  } else {
+    dropdown.classList.add('dropdown-content-off');
+    dropdown.classList.remove('dropdown-content');
+    button.classList.remove('pressed');
+  }
 }
 
-function closeAllDropdowns() {
-    var dropdowns = document.querySelectorAll('.dropdown-content');
-    var buttons = document.querySelectorAll('.ButtProduct');
-    for (var i = 0; i < dropdowns.length; i++) {
-        dropdowns[i].style.display = "none";
-        buttons[i].classList.remove('pressed'); 
-    }
-}
+var dropdownContents = document.querySelectorAll('.dropdown-content');
+dropdownContents.forEach(function (dropdown) {
+  dropdown.classList.remove('dropdown-content');
+  dropdown.classList.add('dropdown-content-off');
+});
+
+
+var buttons = document.querySelectorAll('.ButtProduct');
+buttons.forEach(function (button) {
+  button.classList.remove('pressed');
+});
