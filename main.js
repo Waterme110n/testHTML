53e127359563d807f1b40f83858cf481edfa2423
Pavel Osadcy <pavelasadchy@gmail.com>
2025-06-25 18:39:04 +0300

    mini pagination + fix article

--- main.js
+++ main.js
@@ -18,8 +18,42 @@ dropdownContents.forEach(function (dropdown) {
   dropdown.classList.add('dropdown-content-off');
 });
 
-
 var buttons = document.querySelectorAll('.ButtProduct');
 buttons.forEach(function (button) {
   button.classList.remove('pressed');
+});
+
+/*pagination*/
+
+const articles = document.querySelectorAll('.recent-activity-card');
+let currentPage = 0;
+const articlesPerPage = 3;
+const totalPages = Math.ceil(articles.length / articlesPerPage);
+
+showPage(currentPage);
+
+function showPage(page) {
+  articles.forEach((article, index) => {
+    if (index >= page * articlesPerPage && index < (page + 1) * articlesPerPage) {
+      article.classList.add('recent-activity-card');
+      article.classList.remove('display-none');
+    } else {
+      article.classList.remove('recent-activity-card');
+      article.classList.add('display-none');
+    }
+  });
+}
+
+document.getElementById('prev').addEventListener('click', () => {
+  if (currentPage > 0) {
+    currentPage--;
+    showPage(currentPage);
+  }
+});
+
+document.getElementById('next').addEventListener('click', () => {
+  if ((currentPage + 1) * articlesPerPage < articles.length) {
+    currentPage++;
+    showPage(currentPage);
+  }
 });
